b823643485416b9ffdc31f2e4682931862552633
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-16 20:18:02 +0330

    Spawn asteroids out of camera view

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -7,9 +7,12 @@ public class AsteroidSpawner : MonoBehaviour
     public AsteroidInstanceController[] asteroids;
     public float spawnInterval;
     private float timeUntilNextSpawn;
+    private Bounds bounds;
     // Start is called before the first frame update
     void Start()
     {
+        bounds = gameObject.GetComponent<Bounds>();
+        timeUntilNextSpawn = spawnInterval;
     }
 
     // Update is called once per frame
@@ -19,7 +22,21 @@ public class AsteroidSpawner : MonoBehaviour
         if (timeUntilNextSpawn <= 0.0f)
         {
             var asteroid = Instantiate(asteroids[Random.Range(0, asteroids.Length)].gameObject);
-            asteroid.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), 0);
+            int spawnZone = Random.Range(0, 4);
+            Vector3 position = Vector3.zero;
+            if (spawnZone / 2 == 0) // Top & Bottom
+                position.x = Random.Range(bounds.spawnLeftX, bounds.spawnRightX);
+            else if (spawnZone == 2) // Right
+                position.x = bounds.spawnRightX;
+            else // Left
+                position.x = bounds.spawnLeftX;
+            if (spawnZone / 2 == 1) // Right & Left
+                position.y = Random.Range(bounds.spawnBottomY, bounds.spawnTopY);
+            else if (spawnZone == 0) // Top
+                position.y = bounds.spawnTopY;
+            else // Bottom
+                position.y = bounds.spawnBottomY;
+            asteroid.transform.position = position;
             asteroid.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
             timeUntilNextSpawn += spawnInterval;
         }
---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    public float spawnTopY, spawnBottomY, spawnRightX, spawnLeftX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cam = Camera.main;
+        spawnTopY = cam.orthographicSize + 1;
+        spawnRightX = cam.orthographicSize * cam.aspect + 1;
+        spawnBottomY = -spawnTopY;
+        spawnLeftX = -spawnRightX;
+    }
+}
