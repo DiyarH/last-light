3b4b587ed271fdcac42cc70b5926ad2d479baf83
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-16 18:36:21 +0330

    Implement asteroid spawning

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public AsteroidInstanceController[] asteroids;
+    public float spawnInterval;
+    private float timeUntilNextSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilNextSpawn -= Time.deltaTime;
+        if (timeUntilNextSpawn <= 0.0f)
+        {
+            var asteroid = Instantiate(asteroids[Random.Range(0, asteroids.Length)].gameObject);
+            asteroid.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), 0);
+            timeUntilNextSpawn += spawnInterval;
+        }
+    }
+}
