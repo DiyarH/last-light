1140d1673da21f906879b13c3f586f67f889da5f
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-17 21:12:56 +0330

    Add text to show power percentage

--- Assets/Scripts/EnergyStorageController.cs
+++ Assets/Scripts/EnergyStorageController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnergyStorageController : MonoBehaviour
 {
+    public float power = 90.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public EnergyStorageController energyStorage;
+    public Text powerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerText.text = $"{String.Format("{0:0.0}", energyStorage.power)}%";
+    }
+}
