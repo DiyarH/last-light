6e9cb47b76bccbedd11def487f95b6b28dbe3f08
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-19 16:18:29 +0330

    Add ability to exit the game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Gameplay");
     }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
 }
