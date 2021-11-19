6125098f9ecfb9f9a117191d823012e13358ca12
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-11-19 18:05:46 +0330

    Add story description & return to main menu after the game

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class UiManager : MonoBehaviour
     public Text gameOverText;
     public Text scoreText;
     public Text timeText;
+    bool gameIsOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,6 @@ public class UiManager : MonoBehaviour
         eventManager.OnAsteroidDestroyed.AddListener(UpdateScoreText);
         eventManager.OnGameWon.AddListener(Win);
     }
-
     private void UpdateScoreText()
     {
         scoreText.text = $"Score: {String.Format("{0:000000}", player.score)}";
@@ -30,15 +31,21 @@ public class UiManager : MonoBehaviour
     {
         powerText.text = $"{String.Format("{0:0.0}", player.power)}%";
         timeText.text = $"Time remaining: {String.Format("{0:00}", (int)(player.remainingTime) / 60)}:{String.Format("{0:00}", (int)(player.remainingTime) % 60)}";
+        if (Input.GetKeyDown(KeyCode.Space) && gameIsOver)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     private void GameOver()
     {
-        gameOverText.text = "You ran out of power.";
+        gameOverText.text = "You ran out of power.\nPress Space to return";
+        gameIsOver = true;
     }
 
     private void Win()
     {
-        gameOverText.text = "You were found by the rescue ship!";
+        gameOverText.text = "You were found by the rescue ship!\nPress Space to return";
+        gameIsOver = true;
     }
 }
